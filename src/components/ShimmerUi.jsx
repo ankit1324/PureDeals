67247d5701dfcd3349c9671108312f241d28e38d
Ankit <ak0330255@gmail.com>
2024-07-27 22:09:19 +0530

    ShimmerUI: Added

---
+++ src/components/ShimmerUi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ShimmerCard from "./ShimmerCard";
+
+const ShimmerUi = () => {
+  return (
+    <div className="mt-5 ml-72 flex flex-wrap">
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+    </div>
+  );
+};
+
+export default ShimmerUi;
